d9ae3b45a7d655028ffd9aa794ddb141d92cf822
Maria Alejandra Gomez Osman <maralego@bancolombia.com.co>
2024-01-30 14:53:54 -0500

    Subscribe method handle

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Flux<String> nombres = Flux.just("Andres", "Pedro", "Diego", "Juan")
+                .doOnNext(System.out::println);
+
+        nombres.subscribe(log::info);
+    }
 }
